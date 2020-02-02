1fd9da02ef8974a4f460e453dffbae60dafacfa9
aceteam.dfs@gmail.com <eugene@Eugenes-MacBook-Pro-2.local>
2020-02-01 22:14:56 -0600

    first commit

---
+++ src/main/java/com/khoubyari/example/domain/Hotel.java
@@ -0,0 +1,104 @@
+package com.khoubyari.example.domain;
+
+import javax.persistence.*;
+import javax.xml.bind.annotation.*;
+
+/*
+ * a simple domain entity doubling as a DTO
+ */
+@Entity
+@Table(name = "hotel")
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Hotel {
+
+    @Id
+    @GeneratedValue()
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column()
+    private String description;
+
+    @Column()
+    String city;
+
+    @Column()
+    private int rating;
+
+    @Column()
+    private boolean isAvailable;
+
+    public Hotel() {
+    }
+
+    public Hotel(String name, String description, int rating, boolean available) {
+        this.name = name;
+        this.description = description;
+        this.rating = rating;
+        this.isAvailable = available;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    // for tests ONLY
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public boolean getIsAvailable() {
+        return isAvailable;
+    }
+
+    public void setIsAvailable(boolean available) {
+        this.isAvailable = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Hotel {" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", city='" + city + '\'' +
+                ", rating=" + rating +
+                ", isAvailable=" + isAvailable +
+                '}';
+    }
+}
